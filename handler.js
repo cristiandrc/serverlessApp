c5c0fde6c2327aaf5de12ef7274e82239fa1d89e
Cristian Rojas <crisdarroca@hotmail.com>
2024-12-04 22:41:27 -0500

    feat: add dynamodb local

--- handler.js
+++ handler.js
@@ -1,8 +1,25 @@
-exports.hello = async (event) => {
+const {
+  ListTablesCommand,
+  DynamoDBClient,
+} = require('@aws-sdk/client-dynamodb')
+
+const clientDB = new DynamoDBClient({
+  region: 'localhost',
+  endpoint: 'http://localhost:8000',
+})
+
+exports.getUsers = async (event) => {
+  const command = new ListTablesCommand({
+    // ExclusiveStartTableName: 'usersTable',
+  })
+
+  const response = await clientDB.send(command)
+  console.log(response.TableNames.join('\n'))
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v4.0! Your function executed successfully!'
-    })
-  };
-};
+      message: `responses:${JSON.stringify(response)}`,
+    }),
+  }
+}
